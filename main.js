0b234a7928b8afcf3aee879a29ff06c66bf16a69
Rg96Dev <rafalgui96@gmail.com>
2024-02-07 09:51:04 -0300

    lista de cadrasto de contatos

---
+++ main.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("contact-list");
+
+let linhas = " ";
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const inputNomeContato = document.getElementById("nome-contato");
+    const inputTelefone = document.getElementById("telefone");
+
+    let linha = "<tr>";
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += "</tr>";
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector("tbody");
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeContato.value = " ";
+    inputTelefone.value = " ";
+
+});
